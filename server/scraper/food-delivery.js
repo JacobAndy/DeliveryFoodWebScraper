8496229a12edb170a02adb9e0fe7acb62f60e88e
Jacob Anderson <odistiinct@gmail.com>
2018-06-29 09:33:50 -0500

    papa johns auto fills county

--- server/scraper/food-delivery.js
+++ server/scraper/food-delivery.js
@@ -9,14 +9,19 @@ const papaJohns = async () => {
   );
   await page.waitFor(4000);
   await page.type("#locations-streetaddress", "2022 Sunnyside court", {
-    delay: 200
+    delay: 30
   });
-  await page.waitFor(200);
+  await page.waitFor(600);
   await page.click("body > div.pac-container > .pac-item:nth-child(1)");
-  await page.waitFor(4000);
-  await page.type("#locations-usa-zipcode", "37027");
+  // await page.waitFor(4000);
+  // this is unnessecary due to when my bot clicks on the first option, it auto fills the zip code.
+  // await page.type("#locations-usa-zipcode", "3");
+
   await page.waitFor(10);
   await page.click("#locations-form > div.button-set > input");
+  await page.waitFor(4000);
+  //Pepperoni Pizza
+  await page.click("#order-button-0-1");
   // await page.click(
   //MENU
   // "body > div.page-wrapper > header > section.nav-main > nav > ul > li:nth-child(1)"
